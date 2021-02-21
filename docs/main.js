a4ed6c164e633eb4d625e24ba17ec88c6d63ad34
ingloryon <ingloryon@gmail.com>
2021-02-21 17:59:20 -0300

    new database logic

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,7 @@ function initApp() {
     console.log('initApp');
     docId = document.URL.split('?')[1];
     if (!docId)
-        return location.replace(location.origin + '?default');
+        return location.replace('?default');
     liveContent(docId);
 }
 var timeoutID;
