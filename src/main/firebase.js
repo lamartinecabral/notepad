a511d72e57916cded90f18d74bc9961fc6033512
Lamartine Cabral <lamartine@beakyn.com>
2023-04-12 00:30:34 -0300

    refactor(main): use new firebase module

--- src/main/firebase.js
+++ src/main/firebase.js
@@ -1,15 +1,19 @@
 // @ts-check
-
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { firebaseConfig } from "../config.js";
 import { State } from "./state.js";
+
+/** @type {import('../firebase')['default']} */
+// @ts-ignore
+const firebase = window.firebase
+
+const { initializeApp } = firebase.app;
+const { getFirestore } = firebase.firestore;
+const { getAuth } = firebase.auth;
+const firebaseConfig = firebase.config;
 
 // Initialize Firebase
 console.log(`init firebase (${State.docId})`);
 
-const app = initializeApp(firebaseConfig, State.docId || '');
+const app = initializeApp(firebaseConfig, State.docId || "");
 
 export const auth = getAuth(app);
 
