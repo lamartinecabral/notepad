5c4d4f93033c1290f042d2b609a42e0595ffc058
lamartinecabral <11859047+lamartinecabral@users.noreply.github.com>
2023-04-18 01:10:39 +0000

    fix(main): tab input (#5)

--- src/main/index.js
+++ src/main/index.js
@@ -1,79 +1,120 @@
 // @ts-check
 
-import { State } from "./control";
+import { Dom, State } from "./control";
 import { Service } from "./service";
 import { debounce } from "./utils";
 
-const handlers = {
-  handleKeydown: function (ev) {
-    if (ev.keyCode !== 9 && ev.key !== "Tab") return;
-    if (ev.ctrlKey || ev.shiftKey || ev.altKey) return;
-    ev.preventDefault();
-    /** @type {HTMLTextAreaElement} */ // @ts-ignore
-    const target = ev.target;
-    target.setRangeText(
-      "\t",
-      target.selectionStart,
-      target.selectionEnd,
-      "end"
-    );
-    target.dispatchEvent(new Event("input"));
+const handlers = [
+  {
+    elemId: "body",
+    event: "keyup",
+    handler: function (ev) {
+      if (ev.key === "Escape" || ev.code === "Escape" || ev.keyCode === 27) {
+        State.showPassword.pub(false);
+        State.showOptions.pub(false);
+      }
+    },
   },
-  handleInput: debounce(function () {
-    State.status.pub("Saving...");
-    Service.save()
-      .then(() => {
-        State.status.pub("");
-      })
-      .catch((err) => {
-        console.error(err);
-        State.status.pub("Protected");
-      });
-  }, 500),
-  handleThemeClick: function () {
-    State.nightMode.pub(!State.nightMode.value);
+  {
+    elemId: "theme-menu",
+    event: "click",
+    handler: function () {
+      State.nightMode.pub(!State.nightMode.value);
+    },
   },
-  handlePasswordClick: function () {
-    State.showPassword.pub(true);
+  {
+    elemId: "password-menu",
+    event: "click",
+    handler: function () {
+      State.showPassword.pub(true);
+    },
   },
-  handleOptionsClick: function () {
-    State.showOptions.pub(true);
+  {
+    elemId: "options-menu",
+    event: "click",
+    handler: function () {
+      State.showOptions.pub(true);
+    },
   },
-  handleBackdropClick: function () {
-    State.showPassword.pub(false);
-    State.showOptions.pub(false);
-  },
-  handleModalClick: function (ev) {
-    ev.stopPropagation();
-  },
-  handleKeyup: function (ev) {
-    if (ev.key === "Escape" || ev.code === "Escape" || ev.keyCode === 27) {
+  {
+    elemId: "backdrop",
+    event: "click",
+    handler: function () {
       State.showPassword.pub(false);
       State.showOptions.pub(false);
-    }
+    },
   },
-  handleProtectedChange: function () {
-    Service.setProtected(!State.protected.value);
+  {
+    elemId: "modal",
+    event: "click",
+    handler: function (ev) {
+      ev.stopPropagation();
+    },
   },
-  handlePublicChange: function () {
-    Service.setPublic(!State.public.value);
+  {
+    elemId: "protected",
+    event: "change",
+    handler: function () {
+      Service.setProtected(!State.protected.value);
+    },
   },
-  handleLogoutClick: function () {
-    Service.logout();
-    State.showOptions.pub(false);
+  {
+    elemId: "public",
+    event: "change",
+    handler: function () {
+      Service.setPublic(!State.public.value);
+    },
   },
-  handleSubmit: function (ev) {
-    ev.preventDefault();
-    Service.login(ev.target[1].value);
-    State.showPassword.pub(false);
-    ev.target[1].value = "";
+  {
+    elemId: "logout",
+    event: "click",
+    handler: function () {
+      Service.logout();
+      State.showOptions.pub(false);
+    },
   },
-};
+  {
+    elemId: "form",
+    event: "submit",
+    handler: function (ev) {
+      ev.preventDefault();
+      Service.login(ev.target[1].value);
+      State.showPassword.pub(false);
+      ev.target[1].value = "";
+    },
+  },
+  {
+    elemId: "textarea",
+    event: "keydown",
+    handler: function (ev) {
+      if (ev.keyCode !== 9 && ev.key !== "Tab") return;
+      if (ev.ctrlKey || ev.shiftKey || ev.altKey) return;
+      ev.preventDefault();
+      document.execCommand("insertText", false, "\t");
+    },
+  },
+  {
+    elemId: "textarea",
+    event: "input",
+    handler: debounce(function () {
+      State.status.pub("Saving...");
+      Service.save()
+        .then(() => {
+          State.status.pub("");
+        })
+        .catch((err) => {
+          console.error(err);
+          State.status.pub("Protected");
+        });
+    }, 500),
+  },
+];
 
-Object.assign(window, handlers);
+handlers.forEach(function(entry){
+  Dom.get(entry.elemId).addEventListener(entry.event, entry.handler);
+})
 
 Service.startLiveDoc();
 Service.startLiveUser();
 
 document.body.hidden = false;
-document.body.addEventListener('keyup', handlers.handleKeyup);
