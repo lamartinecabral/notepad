261318c3f7fc306582db2832d1a02b0922758d40
Lamartine Cabral <lamartine@beakyn.com>
2023-04-18 03:37:37 -0300

    feat(main): link to raw doc

--- src/main/index.js
+++ src/main/index.js
@@ -9,28 +9,28 @@ const handlers = [
     elemId: "body",
     event: "keyup",
     handler: function (ev) {
-      if (ev.key === "Escape" || ev.code === "Escape" || ev.keyCode === 27) {
+      if (ev.key === "Escape" || ev.keyCode === 27) {
         State.showPassword.pub(false);
         State.showOptions.pub(false);
       }
     },
   },
   {
-    elemId: "theme-menu",
+    elemId: "theme",
     event: "click",
     handler: function () {
       State.nightMode.pub(!State.nightMode.value);
     },
   },
   {
-    elemId: "password-menu",
+    elemId: "password",
     event: "click",
     handler: function () {
       State.showPassword.pub(true);
     },
   },
   {
-    elemId: "options-menu",
+    elemId: "options",
     event: "click",
     handler: function () {
       State.showOptions.pub(true);
@@ -87,10 +87,21 @@ const handlers = [
     elemId: "textarea",
     event: "keydown",
     handler: function (ev) {
-      if (ev.keyCode !== 9 && ev.key !== "Tab") return;
       if (ev.ctrlKey || ev.shiftKey || ev.altKey) return;
-      ev.preventDefault();
-      document.execCommand("insertText", false, "\t");
+      if (ev.keyCode === 9 || ev.key === "Tab") {
+        ev.preventDefault();
+        return document.execCommand("insertText", false, "\t");
+      } else if (ev.keyCode === 13 || ev.key === "Enter") {
+        ev.preventDefault();
+        let ident = "";
+        for (let j = ev.target.selectionStart; j;) {
+          let char = ev.target.value[--j];
+          if (char === "\n") break;
+          if (char === " " || char === "\t") ident = char + ident;
+          else ident = "";
+        }
+        return document.execCommand("insertText", false, "\n" + ident);
+      }
     },
   },
   {
@@ -110,9 +121,9 @@ const handlers = [
   },
 ];
 
-handlers.forEach(function(entry){
+handlers.forEach(function (entry) {
   Dom.get(entry.elemId).addEventListener(entry.event, entry.handler);
-})
+});
 
 Service.startLiveDoc();
 Service.startLiveUser();
