cd50837e562973c0a5bbfb5a1772b081678dfe61
lamartinecabral <lamartine.cb@gmail.com>
2024-03-03 17:13:33 -0300

    chore: build-data

--- build-data.js
+++ build-data.js
@@ -5,12 +5,15 @@ function run() {
   const buffer = fs.readFileSync(filepath);
   let fileContent = buffer.toString();
 
-  fileContent += "<!-- BUILD DATA\n";
-  fileContent += `buildTime: ${new Date().toISOString()}\n`;
-  fileContent += `GITHUB_SHA: ${process.env.GITHUB_SHA}\n`;
-  fileContent += `GITHUB_WORKFLOW_SHA: ${process.env.GITHUB_WORKFLOW_SHA}\n`;
-  fileContent += `GITHUB_REF: ${process.env.GITHUB_REF}\n`;
-  fileContent += "-->\n";
+  if (process.argv[2] !== "0") {
+    fileContent += "\n<!-- BUILD DATA";
+    fileContent += `\nbuildTime: ${new Date().toISOString()}`;
+    fileContent += `\nGITHUB_SHA: ${process.env.GITHUB_SHA}`;
+    fileContent += `\nGITHUB_REF: ${process.env.GITHUB_REF}`;
+    fileContent += "\n-->";
+  } else {
+    fileContent = fileContent.replace(/\n<!-- BUILD DATA(.|\n)*?-->/, "");
+  }
 
   fs.writeFileSync(filepath, fileContent);
 }
