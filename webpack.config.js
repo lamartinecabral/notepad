3053f67e8dfd80c9ba984dcefd09226afe73c0b9
lamartinecabral <lamartine.cb@gmail.com>
2025-05-22 11:16:44 -0300

    chore: improve build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-module.exports = {
+const entries = {
   entry: {
-    "js/main": "./src/main/index.js",
     "js/my": "./src/my/index.js",
     "js/markdown": "./src/markdown/index.js",
     "js/html": "./src/html/index.js",
-    "js/drive": "./src/drive/index.js",
     "js/code": "./src/code/index.js",
     "js/qrcode": "./src/qrcode/index.js",
     "js/codemirror": "./src/codemirror/index.js",
     "js/firebase": "./src/firebase/index.js",
+  },
+};
+
+const es5Entries = {
+  entry: {
+    "js/main": "./src/main/index.js",
+    "js/drive": "./src/drive/index.js",
     "js/firebase8": "./src/firebase8/index.js",
   },
   module: {
@@ -21,3 +26,5 @@ module.exports = {
   },
   target: ["es5", "web"],
 };
+
+module.exports = [entries, es5Entries];
