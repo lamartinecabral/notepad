a5bdaeda475f582811eef2ebe5e6d8878f06e440
lamartinecabral <lamartine.cb@gmail.com>
2025-05-31 12:47:56 -0300

    chore: improve boilerplate code

--- src/code/boilerplate.js
+++ src/code/boilerplate.js
@@ -27,7 +27,7 @@ const codeBoilerplate = ({ language, source }) => {
         `import "https://cdn.jsdelivr.net/npm/react@18/umd/react.development.js"; import "https://cdn.jsdelivr.net/npm/react-dom@18/umd/react-dom.development.js";\n` +
         `${source}\n` +
         `try { ReactDOM.createRoot(document.getElementById("root")).render(React.createElement(App, null)); }` +
-        `catch (e) { document.getElementById("root").innerHTML = '<pre style="color:red;">' + e.name + ': ' + e.message + '</pre>';}` +
+        `catch (e) { document.getElementById("root").innerHTML = '<pre style="color:red;">' + String(e) + '</pre>';}` +
         `</script></body>`
       );
     }
