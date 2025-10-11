b6c96d5f7353613ff27c78fbfe9d889d9b424a6b
lamartinecabral <lamartine.cb@gmail.com>
2025-10-11 13:53:59 -0300

    [skip ci] improve build data

--- scripts/add-build-data.js
+++ scripts/add-build-data.js
@@ -2,14 +2,21 @@
 const fs = require("fs");
 function run() {
   const filepath = "dist/index.html";
-  const buffer = fs.readFileSync(filepath);
-  let fileContent = buffer.toString();
+  let fileContent = fs.readFileSync(filepath).toString();
 
   if (process.argv[2] !== "0") {
+    const sha =
+      process?.env?.GITHUB_SHA ||
+      require("child_process")
+        .execSync("git log -n 1")
+        .toString()
+        .split("\n")[0]
+        .split(" ")[1];
+
     fileContent += "\n<!-- BUILD DATA";
-    fileContent += `\nbuildTime: ${new Date().toISOString()}`;
-    fileContent += `\nGITHUB_SHA: ${process.env.GITHUB_SHA}`;
-    fileContent += `\nGITHUB_REF: ${process.env.GITHUB_REF}`;
+    fileContent += `\nBUILD_TIME: ${new Date().toISOString()}`;
+    fileContent += `\nSHA: ${sha}`;
+    fileContent += `\nCI: ${process?.env?.CI || false}`;
     fileContent += "\n-->";
   } else {
     fileContent = fileContent.replace(/\n<!-- BUILD DATA(.|\n)*?-->/, "");
